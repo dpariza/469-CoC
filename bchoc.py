d90b31f409d660f9bf39de6cc7a4afaf8e5e71dd
Dane Pariza <danepariza01@gmail.com>
2023-04-21 18:56:53 -0700

    added check for path from environment var

--- bchoc.py
+++ bchoc.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+
+env_path = os.environ.get("BCHOH_FILE_PATH")
+
+if env_path is None:
+    filepath_to_chain = "bchoh_data"
+else:
+    filepath_to_chain = env_path
+
+print(filepath_to_chain)
 
 num_args = len(sys.argv)
 
@@ -10,6 +20,3 @@ if num_args <= 1:
 else:
     command = sys.argv[1]
     print(command)
-
-
-
