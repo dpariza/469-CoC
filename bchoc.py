a370c5ad7eca25b949ba832ae425dfc1e117c80f
Dane Pariza <danepariza01@gmail.com>
2023-04-24 20:34:04 -0700

    huge refactor: made Blockchain class; checkout works now

--- bchoc.py
+++ bchoc.py
@@ -8,6 +8,7 @@ import time
 import argtest
 from datetime import datetime
 import uuid
+import copy
 
 
 class Block:
@@ -31,6 +32,10 @@ class Block:
 			self.evidence_id, self.state, self.data_length, self.data)
 
 
+def get_time():
+	return round(time.time(), 6)
+
+
 def load_file():
 	working_dir = os.getcwd()
 	env_path = os.environ.get("BCHOC_FILE_PATH")
@@ -47,65 +52,16 @@ def load_file():
 	return filepath_to_chain
 
 
-def setup_blockchain():
-	filepath = load_file()
-
-	blockchain = []
-
-	if os.stat(filepath).st_size == 0:
-
-		# If file is empty, init block is created and written to file
-		init_block = Block()
-		blockchain.append(init_block)
-
-		init_bytes = pack_bytes(init_block)
-
-		with open(filepath, 'wb') as f:
-			f.write(init_bytes)
-
-		# False flag denotes that initial block was created and added to the new file
-		return blockchain, False
-
-	else:
-
-		blockchain = parse_file(filepath)
-
-		# True flag denotes successful load of existing init block
-		return blockchain, True
-
-
-def parse_file(filepath):
-	blockchain = []
-
-	# unpacks init block
-	with open(filepath, 'rb') as f:
-		info = f.read(90)
-
-		# unpacks init block, data length preset as 14
-		blockchain.append(unpack_bytes(info, 14))
-
-		# this works for checking following blocks, make it less ugly
-		while continues := f.peek(76):
-			next_data_length = continues[72:76]
-			next_data_length = struct.unpack('I', next_data_length)[0]
-
-			lookahead = f.read(76 + next_data_length)
-
-			blockchain.append(unpack_bytes(lookahead, next_data_length))
-
-		return blockchain
-
-
 def unpack_bytes(unpack_this, data_length):
 	unpacked = struct.unpack(f'32s d 16s I 12s I {data_length}s', unpack_this)
 
-	hash_unpacked = str(hex(int.from_bytes(unpacked[0], "little")))
-	hash_unpacked = hash_unpacked[2:]
+	hash_unpacked = str(hex(int.from_bytes(unpacked[0], "little")))[2:]
+
 	if hash_unpacked == '0':
 		hash_unpacked = '0000000000000000000000000000000000000000000000000000000000000000'
 
-	case_unpacked = str(hex(int.from_bytes(unpacked[2], "little")))
-	case_unpacked = case_unpacked[2:]
+	case_unpacked = str(hex(int.from_bytes(unpacked[2], "little")))[2:]
+
 	if case_unpacked == '0':
 		case_unpacked = '00000000000000000000000000000000'
 
@@ -115,16 +71,6 @@ def unpack_bytes(unpack_this, data_length):
 						 unpacked[6].decode("utf-8").rstrip('\x00'))
 
 	return return_block
-
-
-def write_file(blockchain):
-	filepath = load_file()
-
-	with open(filepath, 'ab') as f:
-		for block in blockchain:
-			if block.new_item:
-				write_bytes = pack_bytes(block)
-				f.write(write_bytes)
 
 
 def pack_bytes(cur):
@@ -139,91 +85,150 @@ def pack_bytes(cur):
 	return return_bytes
 
 
-def new_block(chain, block_to_add):
-	# do all the checks for appending the chain here
-	chain.append(block_to_add)
+def parse_file(filepath):
+	chain = []
+
+	# unpacks init block
+	with open(filepath, 'rb') as f:
+		info = f.read(90)
+
+		# unpacks init block, data length preset as 14
+		chain.append(unpack_bytes(info, 14))
+
+		# this works for checking following blocks, make it less ugly
+		while continues := f.peek(76):
+			next_data_length = continues[72:76]
+			next_data_length = struct.unpack('I', next_data_length)[0]
+
+			lookahead = f.read(76 + next_data_length)
+
+			chain.append(unpack_bytes(lookahead, next_data_length))
+
+		return chain
 
 
-# print(block_to_add)
+def setup_blockchain():
+	filepath = load_file()
 
-def add(chain, args):
-	previous_hash = calculate_hash(chain)
-	timestamp = round(time.time(), 6)
-	case_id = args.case_id
-	evidence_id = args.item_id[0]
-	state = 'CHECKEDIN'
-	data_length = 0
-	data = ''
+	chain = []
 
-	block_to_add = Block(previous_hash, timestamp, case_id, evidence_id, state, data_length, data)
-	block_to_add.new_item = True
-	new_block(chain, block_to_add)
+	if os.stat(filepath).st_size == 0:
 
+		# If file is empty, init block is created and written to file
+		init_block = Block()
+		chain.append(init_block)
 
-def calculate_hash(chain):
-	prev = chain[-1]
-	raw_hashing = prev.previous_hash + str(prev.timestamp) + prev.case_id + str(prev.evidence_id) + prev.state + str(
-		prev.data_length) + prev.data
-	raw_hashing = raw_hashing.encode('utf-8')
-	prev_hash = hashlib.sha256(raw_hashing).digest()
-	prev_hash = str(hex(int.from_bytes(prev_hash, "little")))
-	return prev_hash
+		init_bytes = pack_bytes(init_block)
 
+		with open(filepath, 'wb') as f:
+			f.write(init_bytes)
 
-def handle_input():
-	setup_result = setup_blockchain()
-	chain = setup_result[0]
-	init_flag = setup_result[1]
+		# False flag denotes that initial block was created and added to the new file
+		return chain, False
 
-	args = argtest.process_commands()
+	else:
 
-	if args.command == "add":
-		# ex: bchoc add -c case_id -i item_id [-i item_id ...]
-		# print("added!")
-		add(chain, args)
+		blockchain = parse_file(filepath)
+
+		# True flag denotes successful load of existing init block
+		return blockchain, True
 
 
-	elif args.command == "checkout":
-		pass
+class Blockchain:
 
-	elif args.command == "checkin":
-		pass
+	def __init__(self):
+		setup = setup_blockchain()
+		self.chain = setup[0]
+		self.init_flag = setup[1]
+		self.error_state = False
+		self.error_message = ''
+		self.args = argtest.process_commands()
 
-	elif args.command == "log":
-		for block in chain:
-			print(block)
-			print()
+	def write_file(self):
+		filepath = load_file()
 
+		with open(filepath, 'ab') as f:
+			for block in self.chain:
+				if block.new_item:
+					write_bytes = pack_bytes(block)
+					f.write(write_bytes)
 
-	elif args.command == "remove":
-		print("remove")
-
-		if args.owner:
-			# ex: bchoc remove -i 6 -y idk -o Chris
-			print()
-		else:
-			# ex: bchoc remove -i 6 -y idk
-			print()
-
-	elif args.command == "init":
-		if init_flag:
+	def init(self):
+		if self.init_flag:
 			print("Blockchain file found with INITIAL block.")
 		else:
 			print("Blockchain file not found. Created INITIAL block.")
 
-	elif args.command == "verify":
-		print()
+	def calculate_hash(self):
+		prev = self.chain[-1]
+		raw_hashing = prev.previous_hash + str(prev.timestamp) + prev.case_id + str(prev.evidence_id) + prev.state + \
+					  str(prev.data_length) + prev.data
+		raw_hashing = raw_hashing.encode('utf-8')
 
-	else:
-		print(f"Unknown command: {args.command}")
-		sys.exit(1)
+		prev_hash = hashlib.sha256(raw_hashing).digest()
+		prev_hash = str(hex(int.from_bytes(prev_hash, "little")))
 
-	return chain
+		return prev_hash
+
+	def new_block(self, block_to_add):
+		# do all the checks for appending the chain here
+		block_to_add.new_item = True
+		self.chain.append(block_to_add)
+
+	def add(self, case_id, evidence_id):
+
+		block_to_add = Block(self.calculate_hash(), get_time(), case_id, evidence_id, 'CHECKEDIN', 0, '')
+
+		self.new_block(block_to_add)
+
+	def checkout(self, evidence_id):
+		located = None
+
+		for block in self.chain:
+			if block.evidence_id == evidence_id:
+				located = block
+
+		if located is not None:
+			if located.state != "CHECKEDOUT":
+				out_block = Block(self.calculate_hash(), get_time(), located.case_id, evidence_id, 'CHECKEDOUT', 0, '')
+
+				self.new_block(out_block)
+				print(out_block)
+			else:
+				print("ERROR: This item is already checked out")
+		else:
+			print("ERROR: item does not exist")
+
+	def checkin(self):
+		pass
+
+	def run_command(self):
+		arg = self.args
+		command = arg.command
+
+		if command == "init":
+			self.init()
+
+		elif command == "log":
+			for block in self.chain:
+				print(block)
+				print()
+
+		elif command == "add":
+			self.add(arg.case_id, arg.item_id[0])
+
+		elif command == "checkout":
+			self.checkout(arg.item_id)
+
+		else:
+			print(f"Unknown command: {self.args.command}")
+			sys.exit(1)
 
 
 def main():
-	chain = handle_input()
-	write_file(chain)
+	driver = Blockchain()
+	driver.run_command()
+	driver.write_file()
 
 
 if __name__ == "__main__":
